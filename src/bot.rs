bcdcfd2fbde2aa193b62a776981b8f8bbc9b2fd1
hardliner66 <hardliner66@gmail.com>
2021-01-15 18:02:52 +0100

    workaround eof crash

--- src/bot.rs
+++ src/bot.rs
@@ -1,6 +1,8 @@
 use super::{ChatMessage, Result};
-use std::sync::mpsc::{Receiver, Sender};
+use std::sync::{mpsc::{Receiver, Sender}, Arc, Mutex};
+use std::time::Duration;
 use std::thread;
+use smol::Timer;
 use twitchchat::{
     messages::Commands,
     runner::{AsyncRunner, Status},
@@ -8,6 +10,11 @@ use twitchchat::{
 };
 
 pub struct Bot;
+
+async fn sleep(dur: Duration) {
+    Timer::new(dur).await;
+}
+
 
 impl Bot {
     pub async fn run(
@@ -19,31 +26,45 @@ impl Bot {
     ) -> Result<()> {
         let connector = twitchchat::connector::smol::Connector::twitch()?;
 
-        let mut runner = AsyncRunner::connect(connector, user_config).await?;
+        let mut tries = 0;
+        let messages_for_chat = Arc::new(Mutex::new(messages_for_chat));
 
-        for channel in channels {
-            runner.join(channel).await?;
+        loop {
+            if let Ok(mut runner) = AsyncRunner::connect(connector.clone(), user_config).await {
+                tries = 0;
+                for channel in channels {
+                    runner.join(channel).await?;
+                }
+
+                if let Ok(_) = self.main_loop(
+                    &mut runner,
+                    messages_for_chat.clone(),
+                    send_incomming_chat_message.clone(),
+                    channels.to_vec(),
+                ).await {
+                        return Ok(());
+                }
+            } else {
+                tries += 1;
+                sleep(Duration::from_secs(tries)).await;
+                if tries > 3 {
+                    panic!("could not connect. retried too often!");
+                }
+            }
         }
-
-        self.main_loop(
-            &mut runner,
-            messages_for_chat,
-            send_incomming_chat_message,
-            channels.to_vec(),
-        )
-        .await
     }
 
     async fn main_loop<'a>(
         &self,
         runner: &mut AsyncRunner,
-        messages_for_chat: Receiver<String>,
+        messages_for_chat: Arc<Mutex<Receiver<String>>>,
         send_incomming_chat_message: Sender<ChatMessage>,
         channels: Vec<String>,
     ) -> Result<()> {
         let mut writer = runner.writer();
         let _quit = runner.quit_handle();
         thread::spawn(move || -> Result<()> {
+            let messages_for_chat = messages_for_chat.lock().unwrap();
             loop {
                 if let Ok(message) = messages_for_chat.recv() {
                     for channel in channels.iter() {
